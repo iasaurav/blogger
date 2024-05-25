e9a4d2006a63489802f4849c4ff2f0541dd37262
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-25 17:58:32 +0530

    datalist.js

--- datalist.js
+++ datalist.js
@@ -2,9 +2,10 @@ const store=[];
 const male=[];
 const female=[];
 const train=[];
-
+const link=[];
 $.each(datp,function(j, data) {
-var ul_dat = ['<option value="'+datp[j]+'">'];
+var ul_dat = ['<option value="'+store[j]+'">'+'<option value="'+male[j]+'">'+'<option value="'+female[j]+'">'+'<option value="'+train[j]+'">'+'<option value="'+link[j]+'">'];
             $("#anrede").append(ul_dat);
    
    });
+            
