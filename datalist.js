2009f580ec712e861febde31fa8d1014cff322ac
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-25 17:51:12 +0530

    datalist.js

---
+++ datalist.js
@@ -0,0 +1,10 @@
+const store=[];
+const male=[];
+const female=[];
+const train=[];
+
+$.each(datp,function(j, data) {
+var ul_dat = ['<option value="'+datp[j]+'">'];
+            $("#anrede").append(ul_dat);
+   
+   });
