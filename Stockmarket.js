6aee53ac64c704f2d221fe4ac7dc1d184681e95d
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-05-26 07:16:50 +0530

    Stockmarket.js

---
+++ Stockmarket.js
@@ -0,0 +1,28 @@
+  
+  
+  function openNewWindo() {
+    // Get the selected value from the input field
+    var selectedValu = document.getElementById("grow").value;
+    // Open a new window with the selected value as the URL
+   
+    
+    window.open("https://groww.in/charts/options/"+selectedValu+"", "_blank");
+     
+    
+    
+    
+  }
+  
+  
+  function openNewWindocrude() {
+    // Get the selected value from the input field
+    var crud = document.getElementById("crude").value;
+    // Open a new window with the selected value as the URL
+   
+    
+    window.open("https://trade.fyers.in/popout/index.html?symbol="+crud+"&resolution=5&theme=light", "_blank");
+     
+    
+    
+    
+  }
